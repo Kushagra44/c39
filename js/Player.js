754542dca8fd44e0a8020bcb36bb1bfe9b416b84
Kushagra44 <80953248+Kushagra44@users.noreply.github.com>
2021-06-12 11:45:04 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,31 @@
+class Player{
+    constructor(){
+        this.name=null
+        this.distance=0
+        this.index=null
+    }
+    getCount(){
+        var kush1=database.ref("playerCount")
+        kush1.on("value",function(data){
+            playerCount=data.val()
+        })
+    }   
+    updateCount(count){
+        database.ref("/").update({
+            playerCount:count
+        })
+    } 
+    update(){
+var playerIndex="players/player"+this.index
+database.ref(playerIndex).set({
+    name:this.name,
+    distance:this.distance
+})
+    }
+  static  getPlayerinfo(){
+        var playerInfo =database.ref("players")
+        playerInfo.on("value",(data)=>{
+            allPlayers=data.val()
+        })
+    }
+}
